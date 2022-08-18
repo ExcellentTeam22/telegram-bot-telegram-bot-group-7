81421985d45f1c1e2169f9fbef40b74e824362ef
Nechama Galili <ntgalili@gmail.com>
2022-08-18 14:37:12 +0300

    palindrome, sqrt, factorial

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 from flask import Flask, request, Response
 import requests
 from sympy import isprime
@@ -25,15 +27,20 @@ def is_prime(n: int):
     return isprime(n)
 
 
-@app.route('/message', methods=["POST"])
-def handle_message():
-    print("got message")
-    input = request.get_json()['message']['text']
-    chat_id = request.get_json()['message']['chat']['id']
 
-    if input.split(" ")[0] != '/prime':
-        message_to_send = "don't know this command, try again"
-    elif len(input.split(" ")) == 2:
+def is_factorial(num: int)->bool:
+    fact =1
+    for i in range(1,num) :
+        if num == fact:
+            return True
+        if num<fact:
+            break
+        fact *=i
+    return False
+
+
+def handle_prime(input: str)->str:
+    if len(input.split(" ")) == 2:
         number = input.split(" ")[1]
         if not number.isnumeric():
             message_to_send = "I'm working just with numbers"
@@ -46,7 +53,77 @@ def handle_message():
             else:
                 message_to_send = "not prime"
     else:
-        message_to_send = "enter the number you want to check after the '/prime'"
+        message_to_send="Enter the number you want to check after the '/prime'"
+    return message_to_send
+
+
+def handle_factorial(input):
+    if len(input.split(" ")) == 2:
+        number = input.split(" ")[1]
+        if not number.isnumeric():
+            message_to_send = "I'm working just with numbers"
+        else:
+            if is_factorial(int(number)):
+                message_to_send = "factorial"
+            else:
+                message_to_send = "not factorial"
+    else:
+        message_to_send="Enter the number you want to check after the '/factorial'"
+    return message_to_send
+
+
+
+def handle_palindrome(input):
+    if len(input.split(" ")) == 2:
+        number = input.split(" ")[1]
+        if not number.isnumeric():
+            message_to_send = "I'm working just with numbers"
+        else:
+            if number == number[::-1]:
+                message_to_send = "palindrome"
+            else:
+                message_to_send = "not palindrome"
+    else:
+        message_to_send="Enter the number you want to check after the '/palindrome'"
+    return message_to_send
+
+
+
+def handle_sqrt(input):
+    if len(input.split(" ")) == 2:
+        number = input.split(" ")[1]
+        if not number.isnumeric():
+            message_to_send = "I'm working just with numbers"
+        else:
+            if math.sqrt(int(number))%1==0:
+                message_to_send = "sqrt"
+            else:
+                message_to_send = "not sqrt"
+    else:
+        message_to_send="Enter the number you want to check after the '/sqrt'"
+    return message_to_send
+
+@app.route('/message', methods=["POST"])
+def handle_message():
+    print("got message")
+    input = request.get_json()['message']['text']
+    chat_id = request.get_json()['message']['chat']['id']
+
+    match input.split(" ")[0]:
+        case '/prime':
+            message_to_send = handle_prime(input)
+        case '/factorial':
+            message_to_send = handle_factorial(input)
+        case '/palindrome':
+            message_to_send = handle_palindrome(input)
+        case '/sqrt':
+            message_to_send = handle_sqrt(input)
+        case _:
+            message_to_send = "don't know this command, try again"  # 0 is the default case if x is not found
+
+
+
+
 
     res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}"
                        .format(TOKEN, chat_id, message_to_send))
