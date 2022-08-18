662c5bfc20edf242cd5f267db80fcb3b98b8d3a5
Naama Kari <naama@gmail.com>
2022-08-18 14:13:33 +0300

    end prime

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sympy import isprime
 
 TOKEN = '5540736039:AAEEhq0rWfMMt8xyUUBoxF_Zd4xWTeCryFQ'
 TELEGRAM_INIT_WEBHOOK_URL = 'https://api.telegram.org/bot{}/setWebhook?url=' \
-                            'https://020b-109-253-166-174.eu.ngrok.io/message'.format(TOKEN)
+                            'https://79c3-82-80-173-170.eu.ngrok.io/message'.format(TOKEN)
 
 
 app = Flask(__name__)
@@ -28,18 +28,25 @@ def is_prime(n: int):
 @app.route('/message', methods=["POST"])
 def handle_message():
     print("got message")
-    number = request.get_json()['message']['text']
+    input = request.get_json()['message']['text']
     chat_id = request.get_json()['message']['chat']['id']
-    if not number.isnumeric():
-        message_to_send = "I'm working just with numbers"
-    else:
-        number = int(number)
-        if is_even(number) and not number == 2:
-            message_to_send = "Come on dude, you know even numbers are not prime!"
-        elif is_prime(number):
-            message_to_send = "prime"
+
+    if input.split(" ")[0] != '/prime':
+        message_to_send = "don't know this command, try again"
+    elif len(input.split(" ")) == 2:
+        number = input.split(" ")[1]
+        if not number.isnumeric():
+            message_to_send = "I'm working just with numbers"
         else:
-            message_to_send = "not prime"
+            number = int(number)
+            if is_even(number) and not input == 2:
+                message_to_send = "Come on dude, you know even numbers are not prime!"
+            elif is_prime(number):
+                message_to_send = "prime"
+            else:
+                message_to_send = "not prime"
+    else:
+        message_to_send = "enter the number you want to check after the '/prime'"
 
     res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}"
                        .format(TOKEN, chat_id, message_to_send))
@@ -47,6 +54,5 @@ def handle_message():
 
 
 if __name__ == '__main__':
-    # response = requests.get(TELEGRAM_INIT_WEBHOOK_URL)
+    response = requests.get(TELEGRAM_INIT_WEBHOOK_URL)
     app.run(port=5002)
-
